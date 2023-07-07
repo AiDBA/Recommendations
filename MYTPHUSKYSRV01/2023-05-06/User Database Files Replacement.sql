bae5eefb4416291f5b925655adeaf002eaf5d2c8
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-07-06 22:28:20 -0700

    Uploaded on 7/7/2023 5:28:24 AM

---
+++ MYTPHUSKYSRV01/2023-05-06/User Database Files Replacement.sql
@@ -0,0 +1,200 @@
+
+EXEC sp_configure 'show advanced options','1';
+RECONFIGURE WITH OVERRIDE;
+EXEC sp_configure 'xp_cmdshell','1';
+RECONFIGURE WITH OVERRIDE;
+EXEC xp_cmdshell 'md "D:\MYTPHUSKYSRV01\SSDE_DATA"';
+EXEC xp_cmdshell 'md "C:\MYTPHUSKYSRV01\SSDE_LOG"';
+
+CREATE TABLE #results (result nvarchar(max))
+
+
+BEGIN TRY
+	USE master
+	IF EXISTS (SELECT * FROM sys.databases WHERE name = 'FusionDW' AND state = 0)
+	BEGIN
+
+		IF (SELECT physical_name FROM sys.master_files WHERE database_id = DB_ID(N'FusionDW') AND FILE_ID = 2) = 'D:\Husky\ShotscopeNX\DatabaseFusionDW_log.ldf'
+		BEGIN
+
+			ALTER DATABASE [FusionDW] SET OFFLINE WITH ROLLBACK IMMEDIATE;
+
+			IF OBJECT_ID(N'tempdb..#results') IS NOT NULL
+			BEGIN
+				TRUNCATE TABLE #results
+			END
+			INSERT INTO #results
+			EXEC master..xp_cmdshell 'copy "D:\Husky\ShotscopeNX\DatabaseFusionDW_log.ldf" "C:\MYTPHUSKYSRV01\SSDE_LOG\DatabaseFusionDW_log.ldf" /Z /Y ';
+			IF EXISTS(SELECT * FROM #results WHERE result LIKE '%1 file(s) copied.%')
+			BEGIN
+				ALTER DATABASE [FusionDW] MODIFY FILE (NAME='FusionDW_log' , FILENAME='C:\MYTPHUSKYSRV01\SSDE_LOG\DatabaseFusionDW_log.ldf');
+				ALTER DATABASE [FusionDW] SET ONLINE;
+				EXEC master..xp_cmdshell 'del "D:\Husky\ShotscopeNX\DatabaseFusionDW_log.ldf"';
+			END
+			ELSE
+			BEGIN
+				ALTER DATABASE [FusionDW] SET ONLINE;
+			END
+			TRUNCATE TABLE #results
+		END
+	END
+END TRY
+BEGIN CATCH
+	IF DB_ID(N'FusionDW') IS NOT NULL
+	BEGIN
+		ALTER DATABASE [FusionDW] SET ONLINE;
+	END
+END CATCH
+
+BEGIN TRY
+	USE master
+	IF EXISTS (SELECT * FROM sys.databases WHERE name = 'FusionOperational' AND state = 0)
+	BEGIN
+
+		IF (SELECT physical_name FROM sys.master_files WHERE database_id = DB_ID(N'FusionOperational') AND FILE_ID = 2) = 'D:\Husky\ShotscopeNX\DatabaseFusionOperational_log.ldf'
+		BEGIN
+
+			ALTER DATABASE [FusionOperational] SET OFFLINE WITH ROLLBACK IMMEDIATE;
+
+			IF OBJECT_ID(N'tempdb..#results') IS NOT NULL
+			BEGIN
+				TRUNCATE TABLE #results
+			END
+			INSERT INTO #results
+			EXEC master..xp_cmdshell 'copy "D:\Husky\ShotscopeNX\DatabaseFusionOperational_log.ldf" "C:\MYTPHUSKYSRV01\SSDE_LOG\DatabaseFusionOperational_log.ldf" /Z /Y ';
+			IF EXISTS(SELECT * FROM #results WHERE result LIKE '%1 file(s) copied.%')
+			BEGIN
+				ALTER DATABASE [FusionOperational] MODIFY FILE (NAME='FusionOperational_log' , FILENAME='C:\MYTPHUSKYSRV01\SSDE_LOG\DatabaseFusionOperational_log.ldf');
+				ALTER DATABASE [FusionOperational] SET ONLINE;
+				EXEC master..xp_cmdshell 'del "D:\Husky\ShotscopeNX\DatabaseFusionOperational_log.ldf"';
+			END
+			ELSE
+			BEGIN
+				ALTER DATABASE [FusionOperational] SET ONLINE;
+			END
+			TRUNCATE TABLE #results
+		END
+	END
+END TRY
+BEGIN CATCH
+	IF DB_ID(N'FusionOperational') IS NOT NULL
+	BEGIN
+		ALTER DATABASE [FusionOperational] SET ONLINE;
+	END
+END CATCH
+
+BEGIN TRY
+	USE master
+	IF EXISTS (SELECT * FROM sys.databases WHERE name = 'ReportServer' AND state = 0)
+	BEGIN
+
+		IF (SELECT physical_name FROM sys.master_files WHERE database_id = DB_ID(N'ReportServer') AND FILE_ID = 1) = 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\ReportServer.mdf'
+		BEGIN
+
+			ALTER DATABASE [ReportServer] SET OFFLINE WITH ROLLBACK IMMEDIATE;
+
+			IF OBJECT_ID(N'tempdb..#results') IS NOT NULL
+			BEGIN
+				TRUNCATE TABLE #results
+			END
+			INSERT INTO #results
+			EXEC master..xp_cmdshell 'copy "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\ReportServer.mdf" "D:\MYTPHUSKYSRV01\SSDE_DATA\ReportServer.mdf" /Z /Y';
+			IF EXISTS(SELECT * FROM #results WHERE result LIKE '%1 file(s) copied.%')
+			BEGIN
+				ALTER DATABASE [ReportServer] MODIFY FILE (NAME='ReportServer' , FILENAME='D:\MYTPHUSKYSRV01\SSDE_DATA\ReportServer.mdf');
+				ALTER DATABASE [ReportServer] SET ONLINE;
+				EXEC master..xp_cmdshell 'del "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\ReportServer.mdf"';
+			END
+			ELSE
+			BEGIN
+				ALTER DATABASE [ReportServer] SET ONLINE;
+			END
+			TRUNCATE TABLE #results
+		END
+	END
+END TRY
+BEGIN CATCH
+	IF DB_ID(N'ReportServer') IS NOT NULL
+	BEGIN
+		ALTER DATABASE [ReportServer] SET ONLINE;
+	END
+END CATCH
+
+BEGIN TRY
+	USE master
+	IF EXISTS (SELECT * FROM sys.databases WHERE name = 'ReportServerTempDB' AND state = 0)
+	BEGIN
+
+		IF (SELECT physical_name FROM sys.master_files WHERE database_id = DB_ID(N'ReportServerTempDB') AND FILE_ID = 1) = 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\ReportServerTempDB.mdf'
+		BEGIN
+
+			ALTER DATABASE [ReportServerTempDB] SET OFFLINE WITH ROLLBACK IMMEDIATE;
+
+			IF OBJECT_ID(N'tempdb..#results') IS NOT NULL
+			BEGIN
+				TRUNCATE TABLE #results
+			END
+			INSERT INTO #results
+			EXEC master..xp_cmdshell 'copy "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\ReportServerTempDB.mdf" "D:\MYTPHUSKYSRV01\SSDE_DATA\ReportServerTempDB.mdf" /Z /Y';
+			IF EXISTS(SELECT * FROM #results WHERE result LIKE '%1 file(s) copied.%')
+			BEGIN
+				ALTER DATABASE [ReportServerTempDB] MODIFY FILE (NAME='ReportServerTempDB' , FILENAME='D:\MYTPHUSKYSRV01\SSDE_DATA\ReportServerTempDB.mdf');
+				ALTER DATABASE [ReportServerTempDB] SET ONLINE;
+				EXEC master..xp_cmdshell 'del "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\ReportServerTempDB.mdf"';
+			END
+			ELSE
+			BEGIN
+				ALTER DATABASE [ReportServerTempDB] SET ONLINE;
+			END
+			TRUNCATE TABLE #results
+		END
+	END
+END TRY
+BEGIN CATCH
+	IF DB_ID(N'ReportServerTempDB') IS NOT NULL
+	BEGIN
+		ALTER DATABASE [ReportServerTempDB] SET ONLINE;
+	END
+END CATCH
+
+BEGIN TRY
+	USE master
+	IF EXISTS (SELECT * FROM sys.databases WHERE name = 'ShotscopeNXSecurity' AND state = 0)
+	BEGIN
+
+		IF (SELECT physical_name FROM sys.master_files WHERE database_id = DB_ID(N'ShotscopeNXSecurity') AND FILE_ID = 1) = 'C:\Program Files\Microsoft SQL Server\MSSQL10.MSSQLSERVER\MSSQL\DATA\ShotscopeNXSecurity.mdf'
+		BEGIN
+
+			ALTER DATABASE [ShotscopeNXSecurity] SET OFFLINE WITH ROLLBACK IMMEDIATE;
+
+			IF OBJECT_ID(N'tempdb..#results') IS NOT NULL
+			BEGIN
+				TRUNCATE TABLE #results
+			END
+			INSERT INTO #results
+			EXEC master..xp_cmdshell 'copy "C:\Program Files\Microsoft SQL Server\MSSQL10.MSSQLSERVER\MSSQL\DATA\ShotscopeNXSecurity.mdf" "D:\MYTPHUSKYSRV01\SSDE_DATA\ShotscopeNXSecurity.mdf" /Z /Y';
+			IF EXISTS(SELECT * FROM #results WHERE result LIKE '%1 file(s) copied.%')
+			BEGIN
+				ALTER DATABASE [ShotscopeNXSecurity] MODIFY FILE (NAME='ShotscopeNXSecurity' , FILENAME='D:\MYTPHUSKYSRV01\SSDE_DATA\ShotscopeNXSecurity.mdf');
+				ALTER DATABASE [ShotscopeNXSecurity] SET ONLINE;
+				EXEC master..xp_cmdshell 'del "C:\Program Files\Microsoft SQL Server\MSSQL10.MSSQLSERVER\MSSQL\DATA\ShotscopeNXSecurity.mdf"';
+			END
+			ELSE
+			BEGIN
+				ALTER DATABASE [ShotscopeNXSecurity] SET ONLINE;
+			END
+			TRUNCATE TABLE #results
+		END
+	END
+END TRY
+BEGIN CATCH
+	IF DB_ID(N'ShotscopeNXSecurity') IS NOT NULL
+	BEGIN
+		ALTER DATABASE [ShotscopeNXSecurity] SET ONLINE;
+	END
+END CATCH
+
+EXEC sp_configure 'xp_cmdshell','0';
+RECONFIGURE WITH OVERRIDE;
+EXEC sp_configure 'show advanced options','0';
+RECONFIGURE WITH OVERRIDE;
