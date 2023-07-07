a8f29b493ee5f69f045b150985baff3110d555d4
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-07-06 22:20:54 -0700

    Delete MYSASQLSRV01 directory

--- MYSASQLSRV01/2023-05-06/Rename sa Login.sql
+++
@@ -1,8 +0,0 @@
-
-				/*** Login "sa" has been changed to "System_Administrator_20230506", and new fake login "SA" has been created with password = "P@s$0.569534w0Rd". ***/
-
-				USE [master];
-				ALTER LOGIN [sa] WITH NAME = System_Administrator_20230506;
-				CREATE LOGIN SA WITH PASSWORD=N'P@s$0.569534w0Rd', DEFAULT_DATABASE=[tempdb], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF;
-
-			
