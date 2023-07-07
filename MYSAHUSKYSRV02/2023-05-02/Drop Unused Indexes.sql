367eeed8758775b6a01545f8013e13f4693ba14a
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-07-06 22:27:16 -0700

    Uploaded on 7/7/2023 5:27:20 AM

---
+++ MYSAHUSKYSRV02/2023-05-02/Drop Unused Indexes.sql
@@ -0,0 +1,36 @@
+
+BEGIN TRY
+	DROP INDEX [IX_AIDBA_PK_OBJECTCACHE]
+	ON [FusionOperational].[dbo].[ObjectCache];
+	PRINT 'INDEX [IX_AIDBA_PK_OBJECTCACHE] ON [FusionOperational].[dbo].[ObjectCache] IS DROPED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_AIDBA_PK_OBJECTCACHE] ON [FusionOperational].[dbo].[ObjectCache] CANNOT BE DROPED.';
+END CATCH;
+
+BEGIN TRY
+	DROP INDEX [IX_AIDBA_4_20220820]
+	ON [FusionOperational].[dbo].[Cycle];
+	PRINT 'INDEX [IX_AIDBA_4_20220820] ON [FusionOperational].[dbo].[Cycle] IS DROPED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_AIDBA_4_20220820] ON [FusionOperational].[dbo].[Cycle] CANNOT BE DROPED.';
+END CATCH;
+
+BEGIN TRY
+	DROP INDEX [IX_AIDBA_PK_DOWNTIMEASSIGNEDREASON]
+	ON [FusionOperational].[dbo].[DowntimeAssignedReason];
+	PRINT 'INDEX [IX_AIDBA_PK_DOWNTIMEASSIGNEDREASON] ON [FusionOperational].[dbo].[DowntimeAssignedReason] IS DROPED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_AIDBA_PK_DOWNTIMEASSIGNEDREASON] ON [FusionOperational].[dbo].[DowntimeAssignedReason] CANNOT BE DROPED.';
+END CATCH;
+
+BEGIN TRY
+	DROP INDEX [IX_AIDBA_8_20220820]
+	ON [FusionOperational].[dbo].[DowntimeAssignedReason];
+	PRINT 'INDEX [IX_AIDBA_8_20220820] ON [FusionOperational].[dbo].[DowntimeAssignedReason] IS DROPED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_AIDBA_8_20220820] ON [FusionOperational].[dbo].[DowntimeAssignedReason] CANNOT BE DROPED.';
+END CATCH;
