a7811d0a537b143b0228098697e8ab89c0fa7459
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-07-06 22:20:24 -0700

    Delete CLUBONSRV directory

--- CLUBONSRV/2023-05-02/DBCC CheckDB.sql
+++
@@ -1,8 +0,0 @@
-/*** This script should be run in CLUBONSRV SQL Server instance. ***/
-					BEGIN TRY
-					DBCC CHECKDB ([FAS_Train]) WITH NO_INFOMSGS;
-					END TRY
-					BEGIN CATCH
-					PRINT ERROR_MESSAGE()
-					END CATCH
-					
