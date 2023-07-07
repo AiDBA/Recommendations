a8c7b272d020748c32c510acfcf9822b362af7b9
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-07-06 22:17:54 -0700

    Uploaded on 7/7/2023 5:17:57 AM

---
+++ MYTPEPTSTSRV01/2023-06-05/Execution Plan Analysis.sql
@@ -0,0 +1,8 @@
+
+/* 
+This Query has been Tuned by Ai-DBA Query Optimizer (v1.4.0.0).
+Copyright (C) 2019-2023 Fard Technology Corp., all rights reserved.
+Date: 6/5/2023 12:50:42 AM UTC
+*/
+BEGIN TRY     CREATE UNIQUE INDEX [IX_AIDBA_PK__FSW_TBLD__3214EC079F6F972D] ON [SWISSLOG].[DBO].[FSW_TBLDELORDERMASTER] ([ID]) WITH (DROP_EXISTING=OFF );
+ END TRY BEGIN CATCH END CATCH;
