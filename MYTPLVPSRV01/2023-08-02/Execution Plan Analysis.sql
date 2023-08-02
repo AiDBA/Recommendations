7661b38e57299b4f1e36f4b822d02ffed164aa81
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-08-02 04:42:09 -0700

    Uploaded on 8/2/2023 11:42:08 AM

---
+++ MYTPLVPSRV01/2023-08-02/Execution Plan Analysis.sql
@@ -0,0 +1,8 @@
+
+/* 
+This Query has been Tuned by Ai-DBA Query Optimizer (v1.4.0.0).
+Copyright (C) 2019-2023 Fard Technology Corp., all rights reserved.
+Date: 8/2/2023 11:39:52 AM UTC
+*/
+BEGIN TRY     CREATE NONCLUSTERED INDEX AIDBA_NCIX_230802113955 ON [LINEVIEW4REPORTS].[DBO].[LOSS] ([DTSTAMP],[MODIFIED]) ;
+ END TRY BEGIN CATCH END CATCH;
