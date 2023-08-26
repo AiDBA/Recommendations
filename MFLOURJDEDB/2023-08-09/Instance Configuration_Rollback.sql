378d1503eff6c2df68d2ed4bea7830d4c4f0c591
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-08-26 12:58:19 -0700

    Uploaded on 8/26/2023 7:58:18 PM

---
+++ MFLOURJDEDB/2023-08-09/Instance Configuration_Rollback.sql
@@ -0,0 +1,45 @@
+
+DECLARE @text nvarchar(max);
+BEGIN TRY
+	EXEC sp_configure'show advanced options', 1;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+BEGIN TRY
+	EXEC sp_configure'optimize for ad hoc workloads', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+BEGIN TRY
+	EXEC sp_configure'max server memory (MB)', 87648;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+
+BEGIN TRY
+	EXEC sp_configure'min server memory (MB)', 10956;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+				
+BEGIN TRY
+	EXEC sp_configure'show advanced options', 0;
+	RECONFIGURE WITH OVERRIDE;
+END TRY
+BEGIN CATCH
+	SELECT @text = ERROR_MESSAGE();
+	PRINT @text;
+END CATCH;
+		
