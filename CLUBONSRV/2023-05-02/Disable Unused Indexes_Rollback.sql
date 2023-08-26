011028dffe7120982c4e91cc6438893cb9c1c71f
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-08-26 12:53:28 -0700

    Uploaded on 8/26/2023 7:53:28 PM

---
+++ CLUBONSRV/2023-05-02/Disable Unused Indexes_Rollback.sql
@@ -0,0 +1,45 @@
+
+BEGIN TRY
+	ALTER INDEX [IX_mrmSalesMaster]
+	ON [ClubOn].[dbo].[mrmSalesMaster] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOn].[dbo].[mrmSalesMaster] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOn].[dbo].[mrmSalesMaster] CANNOT BE ENABLED.';
+END CATCH;
+
+BEGIN TRY
+	ALTER INDEX [IX_mrmSalesMaster]
+	ON [ClubOnTest].[dbo].[mrmSalesMaster] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOnTest].[dbo].[mrmSalesMaster] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOnTest].[dbo].[mrmSalesMaster] CANNOT BE ENABLED.';
+END CATCH;
+
+BEGIN TRY
+	ALTER INDEX [IX_mrmSalesMaster]
+	ON [ClubOn_Train].[dbo].[mrmSalesMaster] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOn_Train].[dbo].[mrmSalesMaster] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOn_Train].[dbo].[mrmSalesMaster] CANNOT BE ENABLED.';
+END CATCH;
+
+BEGIN TRY
+	ALTER INDEX [IX_mrmSalesMaster]
+	ON [ClubOn99].[dbo].[mrmSalesMaster] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOn99].[dbo].[mrmSalesMaster] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOn99].[dbo].[mrmSalesMaster] CANNOT BE ENABLED.';
+END CATCH;
+
+BEGIN TRY
+	ALTER INDEX [IX_mrmSalesMaster]
+	ON [ClubOnTest19].[dbo].[mrmSalesMaster] REBUILD WITH (SORT_IN_TEMPDB = ON);
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOnTest19].[dbo].[mrmSalesMaster] IS ENABLED.';
+END TRY
+BEGIN CATCH
+	PRINT 'INDEX [IX_mrmSalesMaster] ON [ClubOnTest19].[dbo].[mrmSalesMaster] CANNOT BE ENABLED.';
+END CATCH;
