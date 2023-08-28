1c355db0c56dde75eff4f244c5a671c670714c3a
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-08-28 05:34:37 -0700

    Uploaded on 8/28/2023 12:34:37 PM

---
+++ CLUBONSRV/2023-08-28/Execution Plan Analysis_Change.sql
@@ -0,0 +1,8 @@
+
+/* 
+This Query has been Tuned by Ai-DBA Query Optimizer (v1.4.0.0).
+Copyright (C) 2019-2023 Fard Technology Corp., all rights reserved.
+Date: 8/28/2023 12:32:46 PM UTC
+*/
+BEGIN TRY     CREATE NONCLUSTERED INDEX AIDBA_NCIX_230828123248 ON [CLUBON].[DBO].[FRECADDYSETUP] ([STATUS]) INCLUDE ([REMARK]) ;
+ END TRY BEGIN CATCH END CATCH;
