b9cb6921fa7ef36a0a87f709144d4ec162d48d16
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-08-28 00:29:48 -0700

    Uploaded on 8/28/2023 7:29:47 AM

---
+++ MYTPQNESRV02/2023-08-26/Database Compression_Rollback.sql
@@ -0,0 +1,10 @@
+ALTER INDEX [iAuditedObject_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						ALTER INDEX [iGCRecord_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						ALTER INDEX [iModifiedOn_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						ALTER INDEX [iNewObject_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						ALTER INDEX [iObjectType_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						ALTER INDEX [iOldObject_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						ALTER INDEX [iOperationType_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						ALTER INDEX [iUserName_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						ALTER INDEX [PK_AuditDataItemPersistent] ON [CHUANSINCACTUS].[dbo].[AuditDataItemPersistent] REBUILD WITH (DATA_COMPRESSION=NONE);
+						
