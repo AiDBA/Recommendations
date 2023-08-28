745fac5f58a5400e2a62489dde1fc96e7e165ee6
AiDBA <132314780+AiDBA@users.noreply.github.com>
2023-08-28 00:30:16 -0700

    Uploaded on 8/28/2023 7:30:16 AM

---
+++ MYSAHUSKYSRV02/2023-08-27/TempDB Files Configuration_Rollback.sql
@@ -0,0 +1,5 @@
+
+ALTER DATABASE [tempdb] ADD FILE (NAME=N'temp3', FILENAME='C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\tempdb_mssql_3.ndf' ,SIZE=8MB TO FILEGROUP [PRIMARY]
+			
+ALTER DATABASE [tempdb] ADD FILE (NAME=N'temp4', FILENAME='C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\tempdb_mssql_4.ndf' ,SIZE=8MB TO FILEGROUP [PRIMARY]
+			
